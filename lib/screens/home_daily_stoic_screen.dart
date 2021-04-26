05b69f69a8887e90d26206631de0eb0aa0ae5d19
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-26 13:10:06 -0500

    create route for calendar screen

--- lib/screens/home_daily_stoic_screen.dart
+++ lib/screens/home_daily_stoic_screen.dart
@@ -4,6 +4,25 @@ import 'package:three_things_flutter/utilities/strings.dart';
 class HomeDailyStoic extends StatelessWidget {
   const HomeDailyStoic({Key key}) : super(key: key);
 //TODO: - ABSTRACT CARDS CODE
+  Widget mainCards(String title, BuildContext context, String routePage) {
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, routePage);
+        },
+        child: SizedBox(
+          height: 60.0,
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Card(
+              child: Center(child: Text(title)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,51 +33,55 @@ class HomeDailyStoic extends StatelessWidget {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
-            child: InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, Strings.gratitudePage);
-              },
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Card(
-                  child: Center(child: Text('Gratitude')),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, Strings.fearPage);
-              },
-              child: SizedBox(
-                height: 60.0,
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Card(
-                    child: Center(child: Text('Fear')),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, Strings.groundingPage);
-              },
-              child: SizedBox(
-                height: 60.0,
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Card(
-                    child: Center(child: Text('Grounding')),
-                  ),
-                ),
-              ),
-            ),
-          ),
+          // Expanded(
+          //   child: InkWell(
+          //     onTap: () {
+          //       Navigator.pushNamed(context, Strings.gratitudePage);
+          //     },
+          //     child: Padding(
+          //       padding: EdgeInsets.all(8.0),
+          //       child: Card(
+          //         child: Center(child: Text('Gratitude')),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          mainCards('Gratitude', context, Strings.gratitudePage),
+          // Expanded(
+          //   child: InkWell(
+          //     onTap: () {
+          //       Navigator.pushNamed(context, Strings.fearPage);
+          //     },
+          //     child: SizedBox(
+          //       height: 60.0,
+          //       child: Padding(
+          //         padding: EdgeInsets.all(8.0),
+          //         child: Card(
+          //           child: Center(child: Text('Fear')),
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          mainCards('Fear', context, Strings.fearPage),
+          // Expanded(
+          //   child: InkWell(
+          //     onTap: () {
+          //       Navigator.pushNamed(context, Strings.groundingPage);
+          //     },
+          //     child: SizedBox(
+          //       height: 60.0,
+          //       child: Padding(
+          //         padding: EdgeInsets.all(8.0),
+          //         child: Card(
+          //           child: Center(child: Text('Grounding')),
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          mainCards('Grounding', context, Strings.groundingPage),
+          mainCards('Calendar', context, Strings.groundingPage)
         ],
       )),
     );
