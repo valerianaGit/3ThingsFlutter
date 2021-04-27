3447251a08bb93ce2ccf2b72ecf1126d5cee079c
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-27 11:57:06 -0500

    correct spelling

--- lib/models/viewModels/gratitude_view_model.dart
+++ lib/models/viewModels/gratitude_view_model.dart
@@ -4,16 +4,16 @@ import 'dart:collection';
 import 'package:three_things_flutter/models/gratitude.dart';
 
 class GratitudeViewModel extends ChangeNotifier {
-  final List<Gratitude> _gratitudeList = [
+  final List<Gratitude> _gratitudesList = [
     Gratitude(
         date: DateTime.now(), entry1: 'family', entry2: 'baloo', entry3: 'home')
   ];
-  UnmodifiableListView<Gratitude> get gratitudeList {
-    return UnmodifiableListView(_gratitudeList);
+  UnmodifiableListView<Gratitude> get gratitudesList {
+    return UnmodifiableListView(_gratitudesList);
   }
 
   int get gratitudesCount {
-    return _gratitudeList.length;
+    return _gratitudesList.length;
   }
 
   void addAnotherGratitude(Gratitude gratitude) {
@@ -22,8 +22,8 @@ class GratitudeViewModel extends ChangeNotifier {
         entry1: gratitude.entry1,
         entry2: gratitude.entry2,
         entry3: gratitude.entry3);
-    _gratitudeList.add(newGratitude);
-    print(_gratitudeList);
+    _gratitudesList.add(newGratitude);
+    print(_gratitudesList);
     notifyListeners();
   }
 }
