b0bc0f2e87e352c40362c42d560ca5febbc03457
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-27 11:48:46 -0500

    create gratitude view model properties and method

--- lib/models/viewModels/gratitude_view_model.dart
+++ lib/models/viewModels/gratitude_view_model.dart
@@ -1,3 +1,29 @@
 import 'package:flutter/cupertino.dart';
+import 'dart:collection';
 
-class GratitudeViewModel extends ChangeNotifier {}
+import 'package:three_things_flutter/models/gratitude.dart';
+
+class GratitudeViewModel extends ChangeNotifier {
+  final List<Gratitude> _gratitudeList = [
+    Gratitude(
+        date: DateTime.now(), entry1: 'family', entry2: 'baloo', entry3: 'home')
+  ];
+  UnmodifiableListView<Gratitude> get gratitudeList {
+    return UnmodifiableListView(_gratitudeList);
+  }
+
+  int get gratitudesCount {
+    return _gratitudeList.length;
+  }
+
+  void addAnotherGratitude(Gratitude gratitude) {
+    Gratitude newGratitude = Gratitude(
+        date: gratitude.date,
+        entry1: gratitude.entry1,
+        entry2: gratitude.entry2,
+        entry3: gratitude.entry3);
+    _gratitudeList.add(newGratitude);
+    print(_gratitudeList);
+    notifyListeners();
+  }
+}
