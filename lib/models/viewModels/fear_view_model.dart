c616a9c10bf506a1dd47cd59e6efa7f88bcad856
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-27 11:58:13 -0500

    create fear properties and methods

--- lib/models/viewModels/fear_view_model.dart
+++ lib/models/viewModels/fear_view_model.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/cupertino.dart';
 
-class FearViewModel extends ChangeNotifier {}
+import 'dart:collection';
+import 'package:three_things_flutter/models/fear.dart';
+
+class FearViewModel extends ChangeNotifier {
+  final List<Fear> _fearsList = [
+    Fear(
+        date: DateTime.now(),
+        define: 'dying',
+        actions: 'not really much',
+        stillAlright: 'Does it matter in the grand scheme of things?')
+  ];
+  UnmodifiableListView<Fear> get fearsList {
+    return UnmodifiableListView(_fearsList);
+  }
+
+  int get fearsCount {
+    return _fearsList.length;
+  }
+
+  void addAnotherGratitude(Fear fear) {
+    Fear newFear = Fear(
+        date: fear.date,
+        define: fear.define,
+        actions: fear.actions,
+        stillAlright: fear.stillAlright);
+    _fearsList.add(newFear);
+    print(_fearsList);
+    notifyListeners();
+  }
+}
