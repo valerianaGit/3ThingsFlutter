cde3f542cc6a0492708e68c607a451e4b7bd3796
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-28 17:27:03 -0500

    create fear and gratitude list files

---
+++ lib/widgets/gratitude_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class GratitudeList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        //   child: Consumer<JournalEntryData>(
+        //   builder: (context, entryData, child) {
+        //     return ListView.builder(
+        //       itemBuilder: (context, index) {
+        //         final entry = entryData.cardList[index];
+
+        //         return JournalCard(
+        //           content: entry.content,
+        //           date: entry.date,
+        //         );
+        //       },
+        //       itemCount: entryData.cardCount,
+        //     );
+        //   },
+        // ),
+        );
+  }
+}
