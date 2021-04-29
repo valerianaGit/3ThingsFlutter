a75c72b05c733178969b7f01fdf3e3304b30e334
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-29 11:33:13 -0500

    update main cards widget

--- lib/widgets/main_cards.dart
+++ lib/widgets/main_cards.dart
@@ -1,47 +1,51 @@
 import 'package:flutter/material.dart';
+
 //TODO: - firgure out if there is a way to MAKE a stateless widget work?
 // ignore: must_be_immutable
-// class MainCard extends StatelessWidget {
-//   late BuildContext context;
-//   final String routePage = '';
-//   final String title = '';
-//   MainCard({title, context, routePage});
-//   @override
-//   Widget build(BuildContext context) {
-//     return Expanded(
-//       child: InkWell(
-//         onTap: () {
-//           Navigator.pushNamed(context, routePage);
-//         },
-//         child: SizedBox(
-//           height: 60.0,
-//           child: Padding(
-//             padding: EdgeInsets.all(8.0),
-//             child: Card(
-//               child: Center(child: Text(title)),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-Widget mainCards(String title, BuildContext context, String routePage) {
-  return Expanded(
-    child: InkWell(
-      onTap: () {
-        Navigator.pushNamed(context, routePage);
-      },
-      child: SizedBox(
-        height: 60.0,
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Card(
-            child: Center(child: Text(title)),
+class MainCard extends StatelessWidget {
+  late BuildContext context;
+  final String routePage;
+  final String title;
+  MainCard(
+      {required this.title, required this.context, required this.routePage});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, routePage);
+        },
+        child: SizedBox(
+          height: 60.0,
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Card(
+              elevation: 5.0,
+              child: Center(child: Text(title)),
+            ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
+
+// Widget mainCards(String title, BuildContext context, String routePage) {
+//   return Expanded(
+//     child: InkWell(
+//       onTap: () {
+//         Navigator.pushNamed(context, routePage);
+//       },
+//       child: SizedBox(
+//         height: 60.0,
+//         child: Padding(
+//           padding: EdgeInsets.all(8.0),
+//           child: Card(
+//             elevation: 5.0,
+//             child: Center(child: Text(title)),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
