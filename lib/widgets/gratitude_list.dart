a363b7f137ff27311c1cfe29b9785e5219bfc341
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-28 21:22:23 -0500

    create gratitudes list for past entries

--- lib/widgets/gratitude_list.dart
+++ lib/widgets/gratitude_list.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:three_things_flutter/models/viewModels/calendar_day_view_model.dart';
+import 'package:three_things_flutter/widgets/stored_values_cards.dart';
 
 class GratitudeList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        //   child: Consumer<JournalEntryData>(
-        //   builder: (context, entryData, child) {
-        //     return ListView.builder(
-        //       itemBuilder: (context, index) {
-        //         final entry = entryData.cardList[index];
+      child: Consumer<CalendarDayViewModel>(
+        builder: (context, entryData, child) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              final entry = entryData.gratitudesList[index];
 
-        //         return JournalCard(
-        //           content: entry.content,
-        //           date: entry.date,
-        //         );
-        //       },
-        //       itemCount: entryData.cardCount,
-        //     );
-        //   },
-        // ),
-        );
+              return StoredValuesCards(
+                  content1: entry.entry1,
+                  content2: entry.entry2,
+                  content3: entry.entry3);
+            },
+            itemCount: entryData.gratitudesCount,
+          );
+        },
+      ),
+    );
   }
 }
+//TODO: - FIX that cards are rendering as tiny little things and you can't read anything
