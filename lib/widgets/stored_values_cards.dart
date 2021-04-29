144d3135dc12e74f66ec1393f03c57abf0451a8a
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-29 11:22:55 -0500

    same fix as past entries screen,

--- lib/widgets/stored_values_cards.dart
+++ lib/widgets/stored_values_cards.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class StoredValuesCards extends StatelessWidget {
-  final String content1 = '';
-  final String content2 = '';
-  final String content3 = '';
-  StoredValuesCards({content1, content2, content3});
+  final String content1;
+  final String content2;
+  final String content3;
+  StoredValuesCards(
+      {required this.content1, required this.content2, required this.content3});
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(
-        child: Text('$content1, $content2, $content3'),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          elevation: 5.0,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text('$content1, $content2, $content3'),
+          ),
+        ),
       ),
     );
   }
