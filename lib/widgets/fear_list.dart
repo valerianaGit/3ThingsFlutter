4def5e20be9205c6a87bf326e7c4ab04ebc88b61
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-28 21:22:08 -0500

    create fears list for past entries reminiscing

--- lib/widgets/fear_list.dart
+++ lib/widgets/fear_list.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:three_things_flutter/models/viewModels/calendar_day_view_model.dart';
+import 'package:three_things_flutter/widgets/stored_values_cards.dart';
 
 class FearList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        //   child: Consumer<JournalEntryData>(
-        //   builder: (context, entryData, child) {
-        //     return ListView.builder(
-        //       itemBuilder: (context, index) {
-        //         final entry = entryData.cardList[index];
-
-        //         return JournalCard(
-        //           content: entry.content,
-        //           date: entry.date,
-        //         );
-        //       },
-        //       itemCount: entryData.cardCount,
-        //     );
-        //   },
-        // ),
-        );
+      child: Consumer<CalendarDayViewModel>(
+        builder: (context, entryData, child) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              final entry = entryData.fearsList[index];
+              return StoredValuesCards(
+                  content1: entry.define,
+                  content2: entry.actions,
+                  content3: entry.stillAlright);
+            },
+            itemCount: entryData.fearsCount,
+          );
+        },
+      ),
+    );
   }
 }
