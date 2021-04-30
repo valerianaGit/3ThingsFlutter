bce3fad445f238fc0ca86d3f1a78848d6200a454
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-29 22:10:40 -0500

    code cleanup

--- lib/screens/calendar_day_screen.dart
+++ lib/screens/calendar_day_screen.dart
@@ -17,9 +17,6 @@ class CalendarDayPage extends StatelessWidget {
               SizedBox(
                 height: 32.0,
               ),
-              //TODO: - Create this circle with the colors in the border
-              //Day circle with colors representing the 3 models
-              //https://stackoverflow.com/a/59964891/7842175
               ClipOval(
                 clipBehavior: Clip.antiAlias,
                 child: Container(
@@ -33,7 +30,7 @@ class CalendarDayPage extends StatelessWidget {
                         height:
                             240.0, // this height forces the container to be a circle
                         child: Text(
-                          "12",
+                          "12", //TODO: - Update with day from data from calendar
                           style: TextStyle(
                             fontSize: 200.0,
                           ),
@@ -46,33 +43,6 @@ class CalendarDayPage extends StatelessWidget {
                   decoration: kGradientBoxDecoration,
                 ),
               ),
-              // ClipOval(
-              //   clipBehavior: Clip.antiAlias,
-              //   child: Container(
-
-              //     decoration: BoxDecoration(
-
-              //       border: Border(
-              //         top: BorderSide(
-              //             width: 40.0, color: Color(0xFFFFFFA500)), //orange
-              //         left: BorderSide(
-              //             width: 40.0, color: Color(0xFFFF6a0dad)), // purple
-              //         right: BorderSide(
-              //             width: 40.0, color: Color(0xFFFF00FF00)), //green
-              //         bottom:
-              //             BorderSide(width: 40.0, color: Color(0xFFFF000000)),
-              //       ),
-              //       //color: Colors.pink,
-              //     ),
-              //     //TODO: Use the day's number date to fill in this text
-
-              //       child: Text(
-              //         '12',
-              //         style: TextStyle(fontSize: 200.0),
-              //       ),
-
-              //   ),
-              // ),
               SizedBox(
                 height: 26.0, // 32 - 8 because cards have edgeinstes.all(8).
               ),
