fcd1911f22e3455774cce33c83f5c5027dfa6247
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-29 21:40:58 -0500

    create circle border constants

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+final kInnerDecoration = BoxDecoration(
+  color: Colors.white,
+  border: Border.all(color: Colors.white),
+  borderRadius: BorderRadius.circular(32),
+);
+
+final kGradientBoxDecoration = BoxDecoration(
+  gradient: LinearGradient(
+      colors: [Colors.yellow.shade600, Colors.orange, Colors.red]),
+  border: Border.all(
+    color: Colors.amber, //kHintColor, so this should be changed?
+  ),
+  borderRadius: BorderRadius.circular(32),
+);
