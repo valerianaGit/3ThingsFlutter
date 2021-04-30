2e8646cb3124119a08e1bb6f890a38738d42fbd9
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-04-29 21:40:36 -0500

    create day circle with gradient border

--- lib/screens/calendar_day_screen.dart
+++ lib/screens/calendar_day_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:three_things_flutter/utilities/strings.dart';
 import 'package:three_things_flutter/widgets/main_cards.dart';
+import 'package:three_things_flutter/utilities/constants.dart';
 
 class CalendarDayPage extends StatelessWidget {
   @override
@@ -13,9 +14,53 @@ class CalendarDayPage extends StatelessWidget {
         child: Container(
           child: Column(
             children: [
-              //TODO: - Create this circle with the colors
+              SizedBox(
+                height: 32.0,
+              ),
+              //TODO: - Create this circle with the colors in the border
               //Day circle with colors representing the 3 models
-              Container(),
+              //https://stackoverflow.com/a/59964891/7842175
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: Text("12", style: TextStyle(fontSize: 200.0)),
+                    decoration: kInnerDecoration,
+                  ),
+                ),
+                //height: 66.0,
+                decoration: kGradientBoxDecoration,
+              ),
+              // ClipOval(
+              //   clipBehavior: Clip.antiAlias,
+              //   child: Container(
+
+              //     decoration: BoxDecoration(
+
+              //       border: Border(
+              //         top: BorderSide(
+              //             width: 40.0, color: Color(0xFFFFFFA500)), //orange
+              //         left: BorderSide(
+              //             width: 40.0, color: Color(0xFFFF6a0dad)), // purple
+              //         right: BorderSide(
+              //             width: 40.0, color: Color(0xFFFF00FF00)), //green
+              //         bottom:
+              //             BorderSide(width: 40.0, color: Color(0xFFFF000000)),
+              //       ),
+              //       //color: Colors.pink,
+              //     ),
+              //     //TODO: Use the day's number date to fill in this text
+
+              //       child: Text(
+              //         '12',
+              //         style: TextStyle(fontSize: 200.0),
+              //       ),
+
+              //   ),
+              // ),
+              SizedBox(
+                height: 26.0, // 32 - 8 because cards have edgeinstes.all(8).
+              ),
               MainCard(
                   title: 'Gratitude',
                   context: context,
