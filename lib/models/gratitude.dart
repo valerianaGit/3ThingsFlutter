7f20f8788a3a94e6326a189fd714a02a136a260e
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-06-30 17:20:02 -0500

    update gratitude model

--- lib/models/gratitude.dart
+++ lib/models/gratitude.dart
@@ -2,41 +2,41 @@ import 'calendar_day.dart';
 
 class Gratitude {
   int? id;
-  DateTime date; //CalendarDay date; // store date only, not time
+  int? dateID; //CalendarDay date; // store date only, not time
   String entry1;
   String entry2;
   String entry3;
 
   Gratitude(
       {this.id,
-      required this.date,
+      this.dateID,
       required this.entry1,
       required this.entry2,
       required this.entry3});
 
-  static final columns = ["id", "date", "entry1", "entry2", "entry3"];
+  static final columns = ["id", "dateID", "entry1", "entry2", "entry3"];
 
   Map<String, dynamic> toMap() {
     final map = Map<String, dynamic>();
     map['entry1'] = entry1;
     map['entry2'] = entry2;
     map['entry3'] = entry3;
-    map['date'] = date;
+    map['dateID'] = dateID;
     map['id'] = id;
     return map;
   }
 
-  static fromMap(Map<String, dynamic> map) {
+  static fromMap(Map map) {
     return Gratitude(
         id: map["id"],
         entry1: map["entry1"],
         entry2: map["entry2"],
         entry3: map["entry3"],
-        date: map["date"]);
+        dateID: map["dateID"]); // DateTime.parse(map["dateID"]));
   }
 
   @override
   String toString() {
-    return 'Gratitude{id: $id, entry1: $entry1, entry2: $entry2, entry3: $entry3, date: $date}';
+    return 'Gratitude{id: $id, entry1: $entry1, entry2: $entry2, entry3: $entry3, dateID: $dateID}';
   }
 }
