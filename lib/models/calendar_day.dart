93896506e18e614ffec235f5439e664430b2afad
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-06-30 17:20:34 -0500

    update calendar day model

--- lib/models/calendar_day.dart
+++ lib/models/calendar_day.dart
@@ -1,15 +1,58 @@
+import 'package:flutter/foundation.dart';
+import 'dart:convert';
 import 'gratitude.dart';
 import 'fear.dart';
-import 'ground.dart';
 
 class CalendarDay {
   int? id;
-  DateTime date = DateTime.now(); // JUST STORE THE DATE, NOT TIME.
-  List<Gratitude>?
-      gratitudeArray; // for these lists, I'll need to serialize into JSON
-  List<Fear>?
-      fearArray; //discussion on serialization here https://stackoverflow.com/a/60506278/7842175
+  DateTime? date;
+  List<Gratitude>? gratitudeArray;
+  // for these lists, I'll need to serialize into JSON string to be stored in SQLite
+  List<Fear>? fearArray;
   bool? groundBool;
 
-  CalendarDay({this.id, this.gratitudeArray, this.fearArray, this.groundBool});
+  CalendarDay(
+      {this.id,
+      this.date,
+      this.gratitudeArray,
+      this.fearArray,
+      this.groundBool});
+
+  static final columns = [
+    "id",
+    "date",
+    "gratitude_array",
+    "fear_array",
+    "ground_bool"
+  ];
+
+  Map<String, dynamic> toMap() {
+    final map = Map<String, dynamic>();
+    map['id'] = id;
+    map['date'] = date;
+    //MARK: List serialization into JSON String to be stored in SQLte database
+    // https://bezkoder.com/dart-flutter-convert-object-to-json-string/
+    map['gratitude_array'] =
+        jsonEncode(gratitudeArray); //SERIALIZE THE ARRAYS INTO JSON strings
+    map['fear_array'] =
+        jsonEncode(fearArray); //SERIALIZE THE ARRAYS INTO JSON strings
+    map['ground_bool'] = groundBool;
+    return map;
+  }
+
+  static fromMap(Map map) {
+    return CalendarDay(
+        id: map['id'],
+        date: DateTime.parse(map['date']),
+        gratitudeArray: jsonDecode(
+            map['gratitude_array']), //Deserialize JSON strings into arrays
+        fearArray: jsonDecode(
+            map['fear_array']), //Deserialize JSON strings into arrays
+        groundBool: map['ground_bool']);
+  }
+
+  @override
+  String toString() {
+    return 'CalendarDay{id: $id, date: $date, gratitude_array: $gratitudeArray, fear_array: $fearArray, ground_bool: $groundBool}';
+  }
 }
