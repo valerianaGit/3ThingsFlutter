7e4e082db07933da24e368fb1387fa5d0b127bad
Valeria Duran de Ruiz <valeria.do@gmail.com>
2021-06-30 17:19:51 -0500

    update fear model

--- lib/models/fear.dart
+++ lib/models/fear.dart
@@ -2,27 +2,27 @@ import 'calendar_day.dart';
 
 class Fear {
   int? id;
-  DateTime
-      date; //CalendarDay date; //DateTime.now(); // store date only, not time
+  int?
+      dateID; //CalendarDay date; //DateTime.now(); // store date only, not time
   String define;
   String actions;
   String stillAlright;
 
   Fear(
       {this.id,
-      required this.date,
+      this.dateID,
       required this.define,
       required this.actions,
       required this.stillAlright});
 
-  static final columns = ["id", "define", "actions", "stillAlright", "date"];
+  static final columns = ["id", "define", "actions", "stillAlright", "dateID"];
 
   Map<String, dynamic> toMap() {
     final map = Map<String, dynamic>();
     map['define'] = define;
     map['actions'] = actions;
     map['stillAlright'] = stillAlright;
-    map['date'] = date;
+    map['dateID'] = dateID;
     //if (id != null) { // took this out because id is required
     map['id'] = id;
     //}
@@ -36,25 +36,20 @@ class Fear {
     //   };
   }
 
-  static fromMap(Map<String, dynamic> map) {
+  static fromMap(Map map) {
+    //<String, dynamic>
     return Fear(
         id: map["id"],
         define: map["define"],
         actions: map["actions"],
         stillAlright: map["stillAlright"],
-        date: map["date"]);
-    // fear.id = map["id"];
-    // fear.define = map["define"];
-    // fear.actions = map["actions"];
-    // fear.stillAlright = map["stillAlright"];
-    // fear.date = map["date"];
-    // return fear;
+        dateID: map["dateID"]); //DateTime.parse(map["date"])
   }
 
   // Implement toString to make it easier to see information about
   // each fear when using the print statement.
   @override
   String toString() {
-    return 'Fear{id: $id, define: $define, actions: $actions, stillAlright: $stillAlright, date: $date}';
+    return 'Fear{id: $id, define: $define, actions: $actions, stillAlright: $stillAlright, dateID: $dateID}';
   }
 }
